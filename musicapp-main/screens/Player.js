f93e8155841a519f3ef6169b60ff40e2ef49422f
emmvi <maryam.altaf.10@gmail.com>
2021-06-03 19:30:29 +0500

    home page updated

--- musicapp-main/screens/Player.js
+++ musicapp-main/screens/Player.js
@@ -1,121 +1,122 @@
 import React from 'react';
 import {
   View,
-  Text, 
-  SafeAreaView, 
-  Image, 
-  ImageBackground, 
-  StyleSheet} from 'react-native';
+  Text,
+  SafeAreaView,
+  Image,
+  ImageBackground,
+  StyleSheet
+} from 'react-native';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import simage from '../assets/song1.png';
 import homebg from '../assets/homebackground.png';
 import Icon from "react-native-vector-icons/MaterialIcons";
 import Icon2 from "react-native-vector-icons/Ionicons";
-export default function Player(){
-   
-    return(
-      <ImageBackground source={homebg} style={styles.backgroundContainer}>
-      <SafeAreaView>
-        <TouchableOpacity style = {styles.icon}>
-        <Icon name="arrow-back-ios" size={28} color="#ffffff" />
-        </TouchableOpacity>
-          <Image source = {simage} style = {styles.image}/>
-          <View style = {styles.view}>
-          <TouchableOpacity>
-              <Icon size = {30} style = {styles.heart} color = "#ffffff" name = "add"/>
-          </TouchableOpacity>
-            <Text style = {styles.title} >Drivers License</Text>
-            <TouchableOpacity>
-              <Icon2 size = {25} style = {styles.heart} color = "#ffffff"name = "ios-heart-outline"/>
-            </TouchableOpacity>
-          </View>
-          <Text style = {styles.artist} >Olivia Rodrigo</Text>
-          <View style = {styles.player}>
-            <TouchableOpacity >
-              <Icon2 name = "ios-shuffle" color = "#ffffff" size = {35} />
-              </TouchableOpacity>
-              <TouchableOpacity >
-              <Icon2 name = "play-skip-back-outline" color = "#ffffff" size = {35} />
-              </TouchableOpacity>
-              <TouchableOpacity >
-              <Icon2 name = "pause-outline" color = "#ffffff" size = {50} />
-              </TouchableOpacity>
-              <TouchableOpacity >
-              <Icon2 name = "play-skip-forward-outline" color = "#ffffff" size = {35} />
-              </TouchableOpacity>
-              <TouchableOpacity >
-              <Icon2 name = "repeat" color = "#ffffff" size = {35} />
-            </TouchableOpacity>
-          </View>
-          
-       
-        
-          
-          
+export default function Player() {
 
-          
+  return (
+    <ImageBackground source={homebg} style={styles.backgroundContainer}>
+      <SafeAreaView>
+        <TouchableOpacity style={styles.icon}>
+          <Icon name="arrow-back-ios" size={28} color="#ffffff" />
+        </TouchableOpacity>
+        <Image source={simage} style={styles.image} />
+        <View style={styles.view}>
+          <TouchableOpacity>
+            <Icon size={30} style={styles.heart} color="#ffffff" name="add" />
+          </TouchableOpacity>
+          <Text style={styles.title} >Drivers License</Text>
+          <TouchableOpacity>
+            <Icon2 size={25} style={styles.heart} color="#ffffff" name="ios-heart-outline" />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.artist} >Olivia Rodrigo</Text>
+        <View style={styles.player}>
+          <TouchableOpacity >
+            <Icon2 name="ios-shuffle" color="#ffffff" size={35} />
+          </TouchableOpacity>
+          <TouchableOpacity >
+            <Icon2 name="play-skip-back-outline" color="#ffffff" size={35} />
+          </TouchableOpacity>
+          <TouchableOpacity >
+            <Icon2 name="pause-outline" color="#ffffff" size={50} />
+          </TouchableOpacity>
+          <TouchableOpacity >
+            <Icon2 name="play-skip-forward-outline" color="#ffffff" size={35} />
+          </TouchableOpacity>
+          <TouchableOpacity >
+            <Icon2 name="repeat" color="#ffffff" size={35} />
+          </TouchableOpacity>
+        </View>
+
+
+
+
+
+
+
       </SafeAreaView>
-      </ImageBackground>
-    )
+    </ImageBackground>
+  )
 
 
 }
 const styles = StyleSheet.create({
   backgroundContainer: {
-      flex: 1,
-      width: null,
-      height: null,
-      // justifyContent: 'center',
-      // alignItems: 'center'
-  },
- 
-  image : {
-  
-   alignSelf : 'center',
-   marginTop : 50,
-   height : 250,
-   width : 250
+    flex: 1,
+    width: null,
+    height: null,
+    // justifyContent: 'center',
+    // alignItems: 'center'
   },
 
-  icon : {
-    marginTop : 20,
-    marginLeft : 15
+  image: {
+
+    alignSelf: 'center',
+    marginTop: 50,
+    height: 250,
+    width: 250
   },
 
-  title : {
-  
-    alignSelf : 'center',
-    color : "#ffffff",
-    fontWeight : "300",
-    fontSize : 20
-    
-  },
-  artist : {
-    alignSelf : 'center',
-    color : "#ffffff",
-    fontWeight : "100",
-    fontSize : 14
-  },
-  
-  view : {
-    marginTop : 12,
-    flexDirection : 'row',
-    justifyContent : 'space-around',
-   
-    
+  icon: {
+    marginTop: 20,
+    marginLeft: 15
   },
 
-  heart : {
-    marginTop : 5  
+  title: {
+
+    alignSelf: 'center',
+    color: "#ffffff",
+    fontWeight: "300",
+    fontSize: 20
+
+  },
+  artist: {
+    alignSelf: 'center',
+    color: "#ffffff",
+    fontWeight: "100",
+    fontSize: 14
   },
 
-  player : {
-    marginTop : 20,
-    alignSelf : 'center',
-    width : 300,
-    flexDirection : 'row',
-    justifyContent : 'space-around',
-    alignItems : 'center'
-    
+  view: {
+    marginTop: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+
+
+  },
+
+  heart: {
+    marginTop: 5
+  },
+
+  player: {
+    marginTop: 20,
+    alignSelf: 'center',
+    width: 300,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+
   }
 });
--- musicapp-main/screens/home.js
+++ musicapp-main/screens/home.js
@@ -15,7 +15,9 @@ import {
     Touchable,
     TouchableOpacity,
     NavigationContainer,
-    Button
+    Button,
+    SafeAreaView,
+    ScrollView
 } from 'react-native';
 
 import bgImage from '../assets/homebackground.png'
@@ -25,16 +27,72 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import TabNavigator from './TabNavigator';
 import Profile from '../screens/profile';
 
-function Home({navigation}){
+import homebg from '../assets/homebackground.png'
+import { FlatList } from 'react-native-gesture-handler';
+
+import song2 from '../assets/song2.jpg'
+
+const { width, height } = Dimensions.get('screen');
+
+function Home({ navigation }) {
     return (
-        <View style={{ flex:1, alignItems:'center', justifyContent: 'center'}}>
-            <Text>Home</Text>
-            <Button title="Go to Search Page" onPress={() => navigation.navigate("Search")}/>
+        // <View style={{ flex:1, alignItems:'center', justifyContent: 'center'}}>
+        //     <Text>Home</Text>
+        //     <Button title="Go to Search Page" onPress={() => navigation.navigate("Search")}/>
 
-            <Button title="Go to Playlist Page" onPress={() => navigation.navigate("Playlist")}/>
+        //     <Button title="Go to Playlist Page" onPress={() => navigation.navigate("Playlist")}/>
 
-            <Button title="Go to Profile Page" onPress={() => navigation.navigate("Profile")}/>
-        </View>
+        //     <Button title="Go to Profile Page" onPress={() => navigation.navigate("Profile")}/>
+        // </View>
+        <ImageBackground source={homebg} style={styles.backgroundContainer}>
+            <SafeAreaView>
+                <ScrollView>   
+                    <View>
+                        <Text style={styles.heading}>Recently Played</Text>
+                        {/* <Image source={albumpic} style={styles.image} /> */}
+                        <ScrollView style={styles.albumTab} horizontal={true}>
+                            <TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity><TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity>
+                        </ScrollView>
+
+                        <Text style={styles.heading}>Most Played</Text>
+                        <ScrollView style={styles.albumTab} horizontal={true}>
+                            <TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity>
+                        </ScrollView>
+
+                        <Text style={styles.heading}>Liked Songs</Text>
+                        <ScrollView style={styles.albumTab} horizontal={true}>
+                            <TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity>
+                        </ScrollView>
+
+                        <Text style={styles.heading}>For You</Text>
+                        <ScrollView style={styles.albumTab} horizontal={true}>
+                            <TouchableOpacity style={styles.editBtn}>
+                                <Image source={song2} style={styles.songpic} />
+                            </TouchableOpacity>
+                        </ScrollView>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </ImageBackground>
+
+
     );
 };
 
@@ -43,8 +101,34 @@ const styles = StyleSheet.create({
         flex: 1,
         width: null,
         height: null,
-        justifyContent: 'center',
-        alignItems: 'center'
+        //justifyContent: 'center',
+        //alignItems: 'center'
+    },
+    heading: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '500',
+        //textAlign: 'center'
+        marginTop: 20,
+        marginLeft: 20
+    },
+
+
+    albumTab: {
+        //flex: 1,
+        //backgroundColor: 'yellow',
+        //padding: 10,
+        flexDirection: 'row',
+        //justifyContent: 'center'
+        //alignSelf: 'center',
+        marginBottom: 20
+    },
+
+    songpic: {
+        width: 100,
+        height: 100,
+        marginLeft: 20,
+        marginTop: 20
     }
 });
 
